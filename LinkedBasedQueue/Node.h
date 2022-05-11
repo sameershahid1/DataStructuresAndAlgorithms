50999f9cb5a5714438e12d0a3760f16df5552fe3
Sameer Shahid <goku.hulk234@gmail.com>
2022-05-11 22:19:08 +0500

    Link List based Queue Uploaded

---
+++ LinkedBasedQueue/Node.h
@@ -0,0 +1,29 @@
+#pragma once
+#include"LQueue.h"
+
+template<class T>
+class Node
+{
+template<class T>
+friend class LQueue;
+private:
+	T data;
+	Node* Next;
+public:
+	Node(T data);
+	Node();
+};
+
+template<class T>
+inline Node<T>::Node(T data)
+{
+	this->data = data;
+	this->Next = nullptr;
+}
+
+template<class T>
+inline Node<T>::Node()
+{
+
+}
+
